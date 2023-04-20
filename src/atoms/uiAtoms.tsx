268ab7dd3a53167472cb6112b2da777c56521347
PabloMur <polillomurillo@gmail.com>
2023-04-20 19:15:47 -0300

    deletd some atoms

--- src/atoms/uiAtoms.tsx
+++ src/atoms/uiAtoms.tsx
@@ -3,18 +3,6 @@ import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
-export const popupPermissionCardState = atom({
-  key: "popupPermissionCard", // unique ID (with respect to other atoms/selectors)
-  default: {
-    mostrado: false,
-  },
-});
-
-export const desplegarMenuAtom = atom({
-  key: "desplegarMenu", // unique ID (with respect to other atoms/selectors)
-  default: false,
-});
-
 export const loaderAtom = atom({
   key: "activarDesactivarLoader",
   default: {
